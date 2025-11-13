27967431da03629cc07e1db0254b120a64cbc1fc
Nef0o0r <159048293+Nef0o0r@users.noreply.github.com>
2025-11-14 00:00:31 +0300

    Update GD step handling and test code comments
    Refactored MainController::fillGDData to set step type and step size based on input data, supporting CONSTANT, COEFFICIENT, and ADAPTIVE step types. Also commented out test code in main.cpp for cleaner execution.

--- Sources/MainController.cpp
+++ Sources/MainController.cpp
@@ -212,22 +212,25 @@ void MainController::fillGDData(const InputData *data)
     m_gdData.function = data->function().toStdString();
     m_gdData.algorithm_type = static_cast<GD::AlgorithmType>(data->extensionId());
     m_gdData.extremum_type = static_cast<GD::ExtremumType>(data->extremumId());
-    m_gdData.step_type = GD::StepType::CONSTANT;
+
     m_gdData.initial_x = data->startX1();
     m_gdData.initial_y = data->startY1();
     m_gdData.x_left_bound = data->minX();
     m_gdData.x_right_bound = data->maxX();
     m_gdData.y_left_bound = data->minY();
     m_gdData.y_right_bound = data->maxY();
-    //После добавления выбора типа шагов (CONST, COEFFICIENT, ADDAPTIVE)
-    //m_gdData.constant_step_size = data->constStep();
-    //m_gdData.constant_step_size = data->adaptiveStep();
-    //m_gdData.coefficient_step_size = data->coefficientStep();
-
-    m_gdData.constant_step_size = data->coefficientStep();
-    m_gdData.coefficient_step_size = data->coefficientStep();
-
+    m_gdData.constant_step_size = data->step();
+    m_gdData.coefficient_step_size = data->step();
     m_gdData.result_precision = data->resultAccuracy();
     m_gdData.computation_precision = data->calcAccuracy();
     m_gdData.max_iterations = data->maxIterations();
+    m_gdData.max_function_calls = data->maxFuncCalls();
+
+    m_gdData.step_type = static_cast<GD::StepType>(data->stepId());
+    if(m_gdData.step_type == GD::StepType::CONSTANT || m_gdData.step_type == GD::StepType::ADAPTIVE){
+        m_gdData.constant_step_size = data->step();
+    } else if(m_gdData.step_type == GD::StepType::COEFFICIENT){
+        m_gdData.coefficient_step_size = data->step();
+    }
+
 }
